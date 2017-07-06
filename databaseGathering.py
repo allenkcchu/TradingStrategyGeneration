9c3b2f471d43bc957fedae6cd85d142e68a85d38
Allen Chu <allen.kcchu@gmail.com>
2017-07-06 10:05:23 +0800

    Data gathering function define

--- databaseGathering.py
+++ databaseGathering.py
@@ -8,7 +8,18 @@ Created on Thu Jul 06 00:40:39 2017
 import pandas as pd;
 import numpy as np;
 import matplotlib.pyplot as plt;
+import grs;
 
-data = pd.read_html('http://isin.twse.com.tw/isin/C_public.jsp?strMode=2');
-data = data[0];
-stock = data[:][0];
+def fetchLatestStockCode():
+    data = pd.read_html('http://isin.twse.com.tw/isin/C_public.jsp?strMode=2');
+    data = data[0];
+    stock = data[:][0];
+    stockCode = [];
+    for i in stock:
+        i = i.encode('utf-8');
+        print(i);
+        if len(i.split('　')) == 2:
+            stockCode.append(i.split('　')[0]);
+
+    return stockCode
+
