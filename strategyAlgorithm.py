f8fc3aa5b9bb9cb2e0f9f0e8124028c8672b83a1
Allen Chu <allen.kcchu@gmail.com>
2017-07-21 18:28:22 +0800

    0721_update

--- strategyAlgorithm.py
+++ strategyAlgorithm.py
@@ -44,22 +44,22 @@ def onBalanceVolume(data,volume,averageLevel):
     return OBV
 
 def Trade(trading,price,dAmount,principal,shareAmount):
+    valid = False;
     fee = 0.001425*price*dAmount;
     if fee <= 0.02:
         fee = 0.02
     tax = 0.003*price*dAmount;
-    if trading == 'buy':
-        tradingFactor = -1;
-        principal = principal+tradingFactor*price*dAmount-fee;
-        shareAmount = shareAmount-tradingFactor*dAmount;
+    if trading == 'buy' and principal >= price*dAmount+fee:
+        principal = principal-price*dAmount-fee;
+        shareAmount = shareAmount+dAmount;
+        valid = True;
     elif trading == 'sell' and shareAmount >= dAmount:
-        tradingFactor = 1;
-        principal = principal+tradingFactor*price*dAmount-fee-tax;
-        shareAmount = shareAmount-tradingFactor*dAmount
-    return (principal,shareAmount)
+        principal = principal+price*dAmount-fee-tax;
+        shareAmount = shareAmount-dAmount
+        valid = True;
+    return (principal,shareAmount,valid)
 
-
-
+plt.close('all');
 database = np.load('170508_Database.npy');
 database = database.item();
 data = database['0056'];
@@ -72,6 +72,8 @@ data = database['0056'];
 #
 #r = np.corrcoef(closePriceArray.astype(np.float));
 
+
+
 averageLevel = 20;
 BBU,BBD = bollingerBand(np.transpose(data[6,:].astype(np.float)),averageLevel);
 volume = np.transpose(data[1,:]).astype(np.float);
@@ -79,35 +81,52 @@ p0 = np.transpose(data[6,averageLevel:].astype(np.float));
 p1 = movingAverage(np.transpose(data[6,:].astype(np.float)),averageLevel);
 p2 = onBalanceVolume(0.5*(np.transpose(data[4,averageLevel:]).astype(np.float)+np.transpose(data[5,averageLevel:]).astype(np.float)),volume,5);
 p3 = (np.transpose(data[6,averageLevel:].astype(np.float))-np.transpose(data[3,averageLevel:].astype(np.float)))/np.transpose(data[6,averageLevel:].astype(np.float));
-p4 = (p0-BBD)/(BBU-BBD)*100;
-position = np.vstack((p0,p1,p2,p3,p4));
-dposition = np.diff(position,1);
-std = np.std(position,1);
-mean = np.mean(position,1);
-pmod = np.zeros(np.shape(position));
-for N in range(np.size(position,0)):
-    pmod[N,:] = (position[N,:]-mean[N])/std[N];
+p4 = BBU-BBD;
+p5 = (p0-BBD)/(BBU-BBD)*100;
+praw = np.vstack((p0,p1,p2,p3,p4,p5));
+dpraw = np.diff(praw,1);
+dpraw = np.hstack((dpraw[:,0].reshape((len(dpraw),1)),dpraw));
+std = np.std(praw,1);
+mean = np.mean(praw,1);
+pmod = np.zeros(np.shape(praw));
+for N in range(np.size(praw,0)):
+    pmod[N,:] = (praw[N,:]-mean[N])/std[N];
 
 
-time1 = [];
-price1 = [];
-time2 = [];
-price2 = [];
+time0 = range(np.size(pmod,1));
+price0 = pmod[0,:];
+C = 1000;
+capital = 1000;
+share = 0;
+timeB = [];
+priceB = [];
+timeS = [];
+priceS = [];
 
-fig=plt.figure();
-plt.ion();
-plt.show();
-for N in range(len(p0)):
-    fig.clear();
-    plt.axis([0,500,-3,3]);
-    
-    time1.append(N);
-    price1.append(pmod[0,N]);
-    if pmod[0,N] <= -1:
-        time2.append(N);
-        price2.append(pmod[0,N]);
-        plt.plot(time1,price1,'k',time2,price2,'ro')
-    else:
-        plt.plot(time1,price1,'k',time2,price2,'ro')
-    
-    plt.pause(0.01);
+#bcon1 = (pmod[0,:]<-1);
+bcon1 = (praw[0,:]>=praw[1,:]);
+bcon2 = (dpraw[1,:]>0);
+bcon = np.vstack((bcon1,bcon2));
+bpts = np.all(bcon,0);
+
+#scon1 = (pmod[0,:]>1);
+scon1 = (praw[0,:]<=praw[1,:])
+scon2 = (dpraw[1,:]<0);
+scon = np.vstack((scon1,scon2));
+spts = np.all(scon,0);
+
+plt.figure();plt.plot(time0,price0);
+for i in time0:
+    if bpts[i]:
+        (capital,share,v) = Trade('buy',praw[0,i],1,capital,share);
+        if v:
+            print(capital);
+            plt.plot(time0[i],price0[i],'bo');
+    if spts[i]:
+        (capital,share,v) = Trade('sell',praw[0,i],1,capital,share);
+        if v:
+            print(capital);
+            plt.plot(time0[i],price0[i],'rx');
+
+print("Return rate = %2.2f%%" % ((capital-C)/C*100));
+print("Share in hand: %d" % share);
