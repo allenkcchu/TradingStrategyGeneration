5f8e9f9bbe10d08265ed9c045debe561abb039c4
Allen Chu <allen.kcchu@gmail.com>
2017-07-31 09:26:00 +0800

    0730_update

--- strategyAlgorithm.py
+++ strategyAlgorithm.py
@@ -59,114 +59,206 @@ def Trade(trading,price,dAmount,principal,shareAmount):
         valid = True;
     return (principal,shareAmount,valid)
 
-plt.close('all');
-database = np.load('170508_Database.npy');
-database = database.item();
-data = database['0056'];
-#closePriceArray = [];
-#for i in sorted(database.keys()):
-#    if len(closePriceArray) == 0:
-#        closePriceArray = np.hstack((closePriceArray,database[i][6,:]));
-#    else:
-#        closePriceArray = np.vstack((closePriceArray,database[i][6,:]));
-#
-#r = np.corrcoef(closePriceArray.astype(np.float));
+def targetFunction(particleParams,plot):
+    # particleParams:Benable/Bbparams/Bcomparison/Senable/Sbparams/Scomparison/level
+    averageLevel = particleParams[6];
 
+    # Standarize data
+    BBU,BBD = bollingerBand(np.transpose(data[6,:].astype(np.float)),averageLevel);
+    volume = np.transpose(data[1,:]).astype(np.float);
+    p0 = np.transpose(data[6,averageLevel:].astype(np.float));
+    p1 = movingAverage(np.transpose(data[6,:].astype(np.float)),averageLevel);
+    p2 = onBalanceVolume(0.5*(np.transpose(data[4,averageLevel:]).astype(np.float)+np.transpose(data[5,averageLevel:]).astype(np.float)),volume,5);
+    p3 = (np.transpose(data[6,averageLevel:].astype(np.float))-np.transpose(data[3,averageLevel:].astype(np.float)))/np.transpose(data[6,averageLevel:].astype(np.float));
+    p4 = BBU-BBD;
+    p5 = (p0-BBD)/(BBU-BBD)*100;
+    praw = np.vstack((p0,p1,p2,p3,p4,p5));
+    dpraw = np.diff(praw,1);
+    dpraw = np.hstack((dpraw[:,0].reshape((len(dpraw),1)),dpraw));
+    std = np.std(praw,1);
+    mean = np.mean(praw,1);
+    pmod = np.zeros(np.shape(praw));
+    for N in range(np.size(praw,0)):
+        pmod[N,:] = (praw[N,:]-mean[N])/std[N];
+    time0 = range(np.size(pmod,1));
 
+    price0 = pmod[0,:];
+    C = 1000;
+    capital = 1000;
+    share = 0;
+    timeB = [];
+    priceB = [];
+    timeS = [];
+    priceS = [];
 
-averageLevel = 20;
-BBU,BBD = bollingerBand(np.transpose(data[6,:].astype(np.float)),averageLevel);
-volume = np.transpose(data[1,:]).astype(np.float);
-p0 = np.transpose(data[6,averageLevel:].astype(np.float));
-p1 = movingAverage(np.transpose(data[6,:].astype(np.float)),averageLevel);
-p2 = onBalanceVolume(0.5*(np.transpose(data[4,averageLevel:]).astype(np.float)+np.transpose(data[5,averageLevel:]).astype(np.float)),volume,5);
-p3 = (np.transpose(data[6,averageLevel:].astype(np.float))-np.transpose(data[3,averageLevel:].astype(np.float)))/np.transpose(data[6,averageLevel:].astype(np.float));
-p4 = BBU-BBD;
-p5 = (p0-BBD)/(BBU-BBD)*100;
-praw = np.vstack((p0,p1,p2,p3,p4,p5));
-dpraw = np.diff(praw,1);
-dpraw = np.hstack((dpraw[:,0].reshape((len(dpraw),1)),dpraw));
-std = np.std(praw,1);
-mean = np.mean(praw,1);
-pmod = np.zeros(np.shape(praw));
-for N in range(np.size(praw,0)):
-    pmod[N,:] = (praw[N,:]-mean[N])/std[N];
+    bcon = [];
+    if np.any(particleParams[0]):
+        for N,i in enumerate(particleParams[0]):
+            if particleParams[0][N]:
+                if particleParams[2][N] == -2:
+                    tmp = (pmod[N,:] <= particleParams[1][N]);
+                elif particleParams[2][N] == -1:
+                    tmp = (pmod[N,:] < particleParams[1][N]);
+                elif particleParams[2][N] == 0:
+                    tmp = (pmod[N,:] == particleParams[1][N]);
+                elif particleParams[2][N] == 1:
+                    tmp = (pmod[N,:] > particleParams[1][N]);
+                elif particleParams[2][N] == 2:
+                    tmp = (pmod[N,:] >= particleParams[1][N]);
 
+                if len(bcon) == 0:
+                    bcon = tmp;
+                else:
+                    bcon = np.vstack((bcon,tmp));
 
-time0 = range(np.size(pmod,1));
-price0 = pmod[0,:];
-C = 1000;
-capital = 1000;
-share = 0;
-timeB = [];
-priceB = [];
-timeS = [];
-priceS = [];
-
-bparams = list([(np.random.randint(0,2,(6))==1),np.random.uniform(-1,1,(6)),np.random.randint(-2,3,(6))]);
-# enable/bparams/comparison
-
-bcon = [];
-if np.any(bparams[0]):
-    for N,i in enumerate(bparams[0]):
-        if bparams[0][N]:
-            if bparams[2][N] == -2:
-                tmp = (pmod[N,:] <= bparams[1][N]);
-            elif bparams[2][N] == -1:
-                tmp = (pmod[N,:] < bparams[1][N]);
-            elif bparams[2][N] == 0:
-                tmp = (pmod[N,:] == bparams[1][N]);
-            elif bparams[2][N] == 1:
-                tmp = (pmod[N,:] > bparams[1][N]);
-            elif bparams[2][N] == 2:
-                tmp = (pmod[N,:] >= bparams[1][N]);
-                
-            if len(bcon) == 0:
-                bcon = tmp;
+        try:
+            if np.size(np.shape(bcon)) == 1:
+                bpts = bcon;
             else:
-                bcon = np.vstack((bcon,tmp));
+                bpts = np.all(bcon,0);
+        except:
+            pass;
+    else:
+        bpts = np.ones((np.size(pmod,1)))==0
 
-    try:
-        if np.size(np.shape(bcon)) == 1:
-            bpts = bcon;
-        else:
-            bpts = np.all(bcon,0);
-    except:
-        pass;
-else:
-    bpts = np.ones((np.size(pmod,1)))==0
-            
-#bcon1 = (pmod[0,:]<bparams[0]);
-#bcon2 = (pmod[1,:]<bparams[1]);
-#bcon3 = (pmod[2,:]<bparams[2]);
-#bcon4 = (pmod[3,:]<bparams[3]);
-#bcon5 = (pmod[4,:]<bparams[4]);
-#bcon6 = (pmod[5,:]<bparams[5]);
-#bcon = np.vstack((bcon1,bcon2,bcon3,bcon4,bcon5,bcon6));
-#bpts = np.all(bcon,0);
+    scon = [];
+    if np.any(particleParams[3]):
+        for N,i in enumerate(particleParams[3]):
+            if particleParams[3][N]:
+                if particleParams[5][N] == -2:
+                    tmp = (pmod[N,:] <= particleParams[4][N]);
+                elif particleParams[5][N] == -1:
+                    tmp = (pmod[N,:] < particleParams[4][N]);
+                elif particleParams[5][N] == 0:
+                    tmp = (pmod[N,:] == particleParams[4][N]);
+                elif particleParams[5][N] == 1:
+                    tmp = (pmod[N,:] > particleParams[4][N]);
+                elif particleParams[5][N] == 2:
+                    tmp = (pmod[N,:] >= particleParams[4][N]);
 
-sparams = np.random.uniform(-3,3,(6));
-scon1 = (pmod[0,:]>sparams[0]);
-scon2 = (pmod[1,:]>sparams[1]);
-scon3 = (pmod[2,:]>sparams[2]);
-scon4 = (pmod[3,:]>sparams[3]);
-scon5 = (pmod[4,:]>sparams[4]);
-scon6 = (pmod[5,:]>sparams[5]);
-scon = np.vstack((scon1,scon2,scon3,scon4,scon5,scon6));
-spts = np.all(scon,0);
+                if len(scon) == 0:
+                    scon = tmp;
+                else:
+                    scon = np.vstack((scon,tmp));
 
-plt.figure();plt.plot(time0,price0);
-for i in time0:
-    if bpts[i]:
-        (capital,share,v) = Trade('buy',praw[0,i],1,capital,share);
-        if v:
-#            print(capital);
-            plt.plot(time0[i],price0[i],'bo');
-    if spts[i]:
-        (capital,share,v) = Trade('sell',praw[0,i],1,capital,share);
-        if v:
-#            print(capital);
-            plt.plot(time0[i],price0[i],'rx');
+        try:
+            if np.size(np.shape(scon)) == 1:
+                spts = scon;
+            else:
+                spts = np.all(scon,0);
+        except:
+            pass;
+    else:
+        spts = np.ones((np.size(pmod,1)))==0
 
-print("Return rate = %2.2f%%" % ((capital-C)/C*100));
-print("Share in hand: %d" % share);
+    if plot == 1:
+        plt.figure();plt.plot(time0,price0);
+    for i in time0:
+        if bpts[i]:
+            (capital,share,v) = Trade('buy',praw[0,i],1,capital,share);
+            if v and plot == 1:
+                plt.plot(time0[i],price0[i],'bo');
+        if spts[i]:
+            (capital,share,v) = Trade('sell',praw[0,i],1,capital,share);
+            if v and plot == 1:
+                plt.plot(time0[i],price0[i],'rx');
+
+    if share > 0:
+        evalReturn = (capital+praw[0,i]*share*0.1-C)/C*100;
+    else:
+        evalReturn = (capital-C)/C*100;
+
+    if plot == 1:
+        print("Return rate = %2.2f%%" % (evalReturn));
+        print("Share in hand: %d" % share);
+
+    return evalReturn
+
+if __name__ == '__main__':
+    plt.close('all');
+    plot = 1;
+    database = np.load('170508_Database.npy');
+    database = database.item();
+    data = database['0056'];
+
+
+    # searchingTime/particleAmount
+    iteration = 5;
+    Period = 50;
+    Number = 20;
+    lowerBound = -5;
+    upperBound = 5;
+    groupRule = list([Period,Number,[lowerBound,upperBound],iteration]);
+    # velocity of bpts/spts
+    Nv = np.random.uniform(-0.09,0.1,(2,6,groupRule[1]));
+    # position of bpts/spts
+    Np = np.random.uniform(-1,1,(2,6,groupRule[1]));
+    # particle personality
+    Nw = np.random.uniform(0,0.5,(2,groupRule[1]));
+    Nw = np.concatenate((np.reshape(1-np.sum(Nw,0),(1,groupRule[1])),Nw),0);
+    # b-enables/b-operators/s-enables/s-operators/statistical level
+    nProperties = [(np.random.randint(0,2,(2,6,groupRule[1]))==1),np.random.randint(-2,3,(2,6,groupRule[1])),np.random.randint(5,51,groupRule[1])];
+
+    # initial best memory
+    NbestPosition = Np;
+    NbestReturn = np.zeros((groupRule[1]));
+    for N in range(groupRule[1]):
+        NbestReturn[N] = targetFunction(list([nProperties[0][0,:,N],Np[0,:,N],nProperties[1][0,:,N],nProperties[0][1,:,N],Np[1,:,N],nProperties[1][1,:,N],nProperties[2][N]]),0);
+
+
+
+
+    if plot == 1:
+        fig = plt.figure();
+    Titerate = 0;
+    while(Titerate<groupRule[3]):
+        T = 0;
+        while(T<groupRule[0]):
+            for N in range(groupRule[1]):
+                particle = list([nProperties[0][0,:,N],Np[0,:,N],nProperties[1][0,:,N],nProperties[0][1,:,N],Np[1,:,N],nProperties[1][1,:,N],nProperties[2][N]]);
+                evalReturn = targetFunction(particle,0);
+                if evalReturn>NbestReturn[N]:
+                    NbestReturn[N] = evalReturn;
+                    NbestPosition[:,:,N] = Np[:,:,N];
+
+
+                for N,i in enumerate(Np+Nv):
+                    for M,j in enumerate(i):
+                        for L,k in enumerate(j):
+                            if k<groupRule[2][0]:
+                                Np[N,M,L] = 2*groupRule[2][0]-k
+                                Nv[N,M,L] = -Nv[N,M,L];
+                            elif k>groupRule[2][1]:
+                                Np[N,M,L] = 2*groupRule[2][1]-k;
+                                Nv[N,M,L] = -Nv[N,M,L];
+                            else:
+                                Np[N,M,L] = k;
+            if plot == 1:
+                fig.clear();
+                plt.plot(Np[0,0,:],Np[1,0,:],'bo',ms = 20);
+                plt.axis([-5,5,-5,5]);
+                plt.pause(0.01);
+            T = T+1;
+#            print(T);
+
+        # Modify velocity
+        selfDv = (NbestPosition-Np)/(groupRule[0]*groupRule[3]);
+        GbestPosition = NbestPosition[:,:,np.where(NbestReturn == np.max(NbestReturn))[0]][:,:,0];
+        GbestPosition = np.reshape(GbestPosition,(np.shape(GbestPosition)[0],np.shape(GbestPosition)[1],1));
+        GbestPosition = np.repeat(GbestPosition,groupRule[1],2);
+        globalDv = (GbestPosition-Np)/(groupRule[0]*groupRule[3]);
+        intefereFactor = np.random.normal(0,np.float(groupRule[2][1])/(groupRule[0]*groupRule[3]),np.shape(Np));
+
+#        Nv = 0.1*Nv+0.3*selfDv+0.6*globalDv;
+        for N in range(groupRule[1]):
+            Nv[:,:,N] = 0.09*Nv[:,:,N] + 0.01*intefereFactor[:,:,N] + 0.3*selfDv[:,:,N] + 0.6*globalDv[:,:,N];
+
+        Titerate = Titerate+1;
+        print("Round %d\n" % (Titerate));
+
+
+    N = np.where(NbestReturn == np.max(NbestReturn))[0][0];
+#    Np = NbestPosition[:,:,N];
+
+particle = list([nProperties[0][0,:,N],NbestPosition[0,:,N],nProperties[1][0,:,N],nProperties[0][1,:,N],NbestPosition[1,:,N],nProperties[1][1,:,N],nProperties[2][N]]);
+evalReturn = targetFunction(particle,1);
